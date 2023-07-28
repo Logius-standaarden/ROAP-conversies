6006fc73e7b4323fc4ffee95196299587c2be0b6
Maarten van Bree <63168360+mvb-nerdzone@users.noreply.github.com>
2023-07-28 18:25:52 +0200

    Initiële versie - zie CHANGELOG.md (#1) (#2)
    * Initiële versie - zie CHANGELOG.md

--- js/config.js
+++ js/config.js
@@ -2,18 +2,22 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   license: "cc-by",
-  specStatus: "DEF",
+  specStatus: "WV",
   specType: "HR",
-  pubDomain: "dk",
-  shortName: "template",
-  publishDate: "2023-06-21",
-  publishVersion: "0.0.3",
+  pubDomain: "ep",
+  shortName: "roap-conversies",
+  publishDate: "2023-07-28",
+  publishVersion: "0.0.1",
   
-  previousVersion: "0.0.2",
-  previousPublishDate: "2022-06-24",
-  previousMaturity: "WV",
-  title: "titleText",
-  content: {"ch01": "informative", "ch02": "", "mermaid": ""},
+  // previousVersion: "0.0.2",
+  // previousPublishDate: "2022-06-24",
+  // previousMaturity: "WV",
+  title: "ROAP conversies",
+  content:
+    {
+        "ROAP-conversies": "informative",
+        "legenda": "informative",
+    },
   editors:
     [
       {
@@ -30,14 +34,14 @@ let respecConfig = {
         companyURL: "https://github.com/Logius-standaarden",
       }
     ],
-  github: "https://github.com/Logius-standaarden/ReSpec-template",
+  github: "https://github.com/Logius-standaarden/ROAP-conversies",
 
 
   // Create PDF and link to file in header (optional):
-  alternateFormats: [
-      {
-          label: "pdf",
-          uri: "template.pdf",
-      },
-  ],
+  // alternateFormats: [
+  //    {
+  //        label: "pdf",
+  //        uri: "template.pdf",
+  //    },
+  // ],
 };
